69f5d2bd21b6c2410996c965253eec55e1203c2a
AlexMattyou <alexmattnsn@gmail.com>
2024-09-13 19:34:01 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,28 @@
+function generateCarousel() {
+  const imagesPath = './data/highlights/';
+  const images = ['0001.png', '0002.png', '0003.png', '0004.png'];
+  const carouselInner = $('#carouselContent');
+  const carouselIndicators = $('#carouselIndicators');
+  
+  images.forEach((img, index) => {
+      const isActive = index === 0 ? 'active' : '';
+      
+      // Create carousel item
+      const carouselItem = `
+          <div class="carousel-item ${isActive}">
+              <img src="${imagesPath}${img}" class="d-block w-100" alt="Image ${index + 1}">
+          </div>
+      `;
+      carouselInner.append(carouselItem);
+      
+      // Create indicator dot
+      const indicatorItem = `
+          <li data-target="#carouselExample" data-slide-to="${index}" class="${isActive}"></li>
+      `;
+      carouselIndicators.append(indicatorItem);
+  });
+}
+
+$(document).ready(function() {
+  generateCarousel();
+});
