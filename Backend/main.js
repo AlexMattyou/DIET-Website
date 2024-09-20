e5b4eddc593a18abbca1c0c6b18257d61cfb4427
AlexMattyou <alexmattnsn@gmail.com>
2024-09-20 14:02:33 +0530

    cors testing 1

--- Backend/main.js
+++ Backend/main.js
@@ -1,9 +1,21 @@
 import express from "express";
 import movieRoutes from "./routes/movie.route.js"
 import connectDB from "./lib/db.js"
+import cors from "cors";
+
+const corsOptions = { 
+	origin: 'https://diettuty.onrender.com', 
+	methods: 'GET,POST,PUT,DELETE', 
+	allowedHeaders: 'Content-Type,Authorization',
+    optionsSuccessStatus: 200
+};
+
 
 const app = express();
 const PORT = 6969;
+
+// only allow the website
+app.use(cors(corsOptions));
 
 // MIDDLEVERSE
 app.use(express.json());
