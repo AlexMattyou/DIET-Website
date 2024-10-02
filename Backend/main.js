23dc82625802228b3504dbc3a220276ec9cf60e4
AlexMattyou <alexmattnsn@gmail.com>
2024-10-02 11:58:49 +0530

    API error

--- Backend/main.js
+++ Backend/main.js
@@ -9,7 +9,7 @@ const app = express();
 const PORT = 5879;
 
 app.use(cors({
-    origin: 'https://diettuty.onrender.com/',  // Replace with the origin where your frontend is hosted
+    origin: 'https://diettuty.onrender.com/admin-zone/',  // Replace with the origin where your frontend is hosted
     credentials: true
   }));
 
